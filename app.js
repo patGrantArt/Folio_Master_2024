dec8c1c28bd2dc33742fadd9676889069084f290
patgrantart <patgrantart@gmail.com>
2024-05-30 14:37:18 +1000

    thu 30th May - successful connection to airtable

--- app.js
+++ app.js
@@ -8,10 +8,48 @@ require('process');
 require('dotenv').config();
 //console.log(process.env);
 
-//airtable config
-//const KEY = process.env.PRIVATE_KEY
-//const BASE_ID = process.env.AIRTABLEBASE
+//AIRTABLE CONFIG
+const Airtable = require('airtable');
+//I don't understand what this one line below does
+const { all } = require('express/lib/application');
+const KEY = process.env.PERSONAL_ACCESS_TOKEN;
+const BASE_ID = process.env.AIRTABLEBASE;
+const base = new Airtable({apiKey: KEY}).base(BASE_ID);
+
+if(base){console.log('Airtable config success')}
+console.log(base);
+
+let masterDataSet = undefined;
 
 
+
+async function getInterviews(){
+    base('interviews').select({
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+    
+        records.forEach(function(record) {
+            console.log('Retrieved', record.get('name'));
+        });
+    
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    });
+    
+
+}
+
+getInterviews();
+
+
+
+
+//server
 app.listen(port, ()=> console.log(`server listening`));
 app.use(express.static('public'));
