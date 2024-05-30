bc73c4f51be10f1d10437c5d72436242525c5f78
patgrantart <patgrantart@gmail.com>
2024-05-30 11:02:44 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log(`hello Will McLean`);
+
+//set up express
+const express = require('express');
+const app = express();
+const port = 3000;
+require('process');
+require('dotenv').config();
+console.log(process.env);
+
+//airtable config
+//const KEY = process.env.PRIVATE_KEY
+//const BASE_ID = process.env.AIRTABLEBASE
+
+
+app.listen(port, ()=> console.log(`server listening`));
+app.use(express.static('public'));
