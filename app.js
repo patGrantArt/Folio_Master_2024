610e306e7ff4a8760788f7fb529f96cd701fc565
patgrantart <patgrantart@gmail.com>
2024-05-30 13:35:03 +1000

    no message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log(`hello Will McLean`);
 //set up express
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 require('process');
 require('dotenv').config();
 //console.log(process.env);
